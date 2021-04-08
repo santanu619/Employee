8bb7e74c5cc20162ffe4a05b50de56e68fbd1533
santanu619 <mohapatra.santanu123@gmail.com>
2021-04-08 11:37:57 +0530

    Added:Code for Salary

--- employeewage.sh
+++ employeewage.sh
@@ -2,13 +2,38 @@ echo "Welcome to Employee Wage Computation Program on Master Branch"
 isFullTime=1
 isAbsent=0
 isPartTime=2
+FullTimeHr=8
+PartTimeHr=4
+SalaryPerHr=20
+totalSalary=0
 attendance=$((RANDOM%3))
-if [ $attendance -eq $isFullTime ]
-then
-	echo "Employee is Present"
-elif [ $attendance -eq $isAbsent ]
-then
-	echo "Employee is Absent"
-else
-	echo "Employee is Part Timer"
-fi
+Attendance()
+{
+	if [ $attendance -eq $isFullTime ]
+	then
+		echo "Employee is Present"
+	elif [ $attendance -eq $isAbsent ]
+	then
+		echo "Employee is Absent"
+	else
+		echo "Employee is Part Timer"
+	fi
+}
+Attendance
+Salary()
+{
+	 if [ $attendance -eq $isFullTime ]
+        then
+                totalSalary=$(($totalSalary+($SalaryPerHr*$FullTimeHr)))
+        elif [ $attendance -eq $isPartTime ]
+        then
+                totalSalary=$(($totalSalary+($SalaryPerHr*$PartTimeHr)))
+        else
+                totalSalary=0
+        fi
+	echo "Salary is: $totalSalary"
+}
+Salary
+
+
+
