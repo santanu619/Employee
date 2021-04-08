ebaa7f3db20c9b96c909d371ab17f2fc5044c3f5
santanu619 <mohapatra.santanu123@gmail.com>
2021-04-08 11:11:28 +0530

    Added: Attendance Taken

--- employeewage.sh
+++ employeewage.sh
@@ -1,1 +1,14 @@
 echo "Welcome to Employee Wage Computation Program on Master Branch"
+isFullTime=1
+isAbsent=0
+isPartTime=2
+attendance=$((RANDOM%3))
+if [ $attendance -eq $isFullTime ]
+then
+	echo "Employee is Present"
+elif [ $attendance -eq $isAbsent ]
+then
+	echo "Employee is Absent"
+else
+	echo "Employee is Part Timer"
+fi
